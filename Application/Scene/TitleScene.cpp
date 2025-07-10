28c42f90c76f3ecef71af92230acb275043859f4
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2025-07-11 01:47:41 +0900

    余分な物削除

--- Application/Scene/TitleScene.cpp
+++ Application/Scene/TitleScene.cpp
@@ -9,25 +9,8 @@ void TitleScene::Initialize() {
     vp_.Initialize();
     vp_.translation_ = {0.0f, 0.0f, -30.0f};
 
-    BaseObjectManager::GetInstance()->LoadAll("CG4");
     debugCamera_ = std::make_unique<DebugCamera>();
     debugCamera_->Initialize(&vp_);
-
-    player_ = std::make_unique<Player>();
-    player_->Init("Player");
-
-    obj_ = std::make_unique<BaseObject>();
-    obj_->Init("test");
-    obj_->CreateModel("animation/BrainStem.gltf");
-
-    followCamera_ = std::make_unique<FollowCamera>();
-    followCamera_->Init();
-    followCamera_->SetTarget(player_->GetWorldTransform());
-
-    player_->SetCamera(followCamera_.get());
-
-    BaseObjectManager::GetInstance()->AddObject(std::move(player_));
-    BaseObjectManager::GetInstance()->AddObject(std::move(obj_));
 }
 
 void TitleScene::Finalize() {
@@ -82,10 +65,7 @@ void TitleScene::CameraUpdate() {
     if (debugCamera_->GetActive()) {
         debugCamera_->Update();
     } else {
-        followCamera_->Update();
-        vp_.matWorld_ = followCamera_->GetViewProjection().matWorld_;
-        vp_.matView_ = followCamera_->GetViewProjection().matView_;
-        vp_.matProjection_ = followCamera_->GetViewProjection().matProjection_;
+        vp_.UpdateMatrix();
     }
 }
 
